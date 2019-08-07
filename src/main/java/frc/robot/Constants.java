926d03c2eb3fef967b3da5ae76f027576f20336c
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-07 17:45:57 -0500

    Put empty values to allow compiling

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,35 +11,35 @@ public final static double OPERATOR_JOYSTICK_DEADZONE = 0.15;
 //CAN Controllers
 //CTRE
 //Drivetrain
-public final static int FRONT_RIGHT_ID = ;
-public final static boolean FRONT_RIGHT_INVERTED = ;
+public final static int FRONT_RIGHT_ID = 0;
+public final static boolean FRONT_RIGHT_INVERTED = false;
 
-public final static int FRONT_LEFT_ID = ;
-public final static boolean FRONT_LEFT_INVERTED = ;
+public final static int FRONT_LEFT_ID = 0;
+public final static boolean FRONT_LEFT_INVERTED = false;
 
-public final static int BACK_RIGHT_ID = ;
-public final static boolean BACK_RIGHT_INVERTED = ;
+public final static int BACK_RIGHT_ID = 0;
+public final static boolean BACK_RIGHT_INVERTED = false;
 
-public final static int BACK_LEFT_ID = ;
-public final static boolean BACK_LEFT_INVERTED = ;
+public final static int BACK_LEFT_ID = 0;
+public final static boolean BACK_LEFT_INVERTED = false;
 
 //Climber Wheels
-public final static int CLIMBER_WHEELS_ID = ;
-public final static boolean CLIMBER_WHEELS_INVERTED = ;
+public final static int CLIMBER_WHEELS_ID = 0;
+public final static boolean CLIMBER_WHEELS_INVERTED = false;
 
 //Sparks
 //Elevator
-public final static int ELEVATOR_RIGHT_ID = ;
-public final static boolean ELEVATOR_RIGHT_INVERTED = ;
+public final static int ELEVATOR_RIGHT_ID = 0;
+public final static boolean ELEVATOR_RIGHT_INVERTED = false;
 
-public final static int ELEVATOR_LEFT_ID = ;
-public final static boolean ELEVATOR_LEFT_INVERTED = ;
+public final static int ELEVATOR_LEFT_ID = 0;
+public final static boolean ELEVATOR_LEFT_INVERTED = false;
 
 //Intake
-public final static int INTAKE_ID = ;
-public final static boolean INTAKE_INVERTED = ;
+public final static int INTAKE_ID = 0;
+public final static boolean INTAKE_INVERTED = false;
 
 //Climber
-public final static int CLIMBER_ID = ;
-public final static boolean CLIMBER_INVERTED = ;
+public final static int CLIMBER_ID = 0;
+public final static boolean CLIMBER_INVERTED = false;
 }
