add30804fe64f7188a279405d605e966656222ec
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-10 11:17:46 -0500

    Loop through components

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,11 +23,6 @@ import frc.team5431.titan.core.robot.Component;
  * project.
  */
 public class Robot extends frc.team5431.titan.core.robot.Robot<Robot> {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
   private Xbox driver;
   private Xbox operator;
 
@@ -40,16 +35,8 @@ public class Robot extends frc.team5431.titan.core.robot.Robot<Robot> {
 
   private List<Component<Robot>> components = List.of();
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-
     climber = new Climber();
     drivebase = new Drivebase();
     elevator = new Elevator();
@@ -60,64 +47,40 @@ public class Robot extends frc.team5431.titan.core.robot.Robot<Robot> {
     components = List.of(climber, drivebase, elevator, intake, teleop, vision);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
+    super.robotPeriodic();
+    components.forEach((com)->com.tick(this));
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    super.autonomousInit();
+    components.forEach((com)->com.init(this));
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+    super.autonomousPeriodic();
+    components.forEach((com)->com.periodic(this));
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
+  @Override
+  public void teleopInit() {
+    super.teleopInit();
+    components.forEach((com)->com.init(this));
+  }
+
   @Override
   public void teleopPeriodic() {
+    super.teleopPeriodic();
+    components.forEach((com)->com.periodic(this));
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
+    super.testPeriodic();
+    components.forEach((com)->com.periodic(this));
   }
 
   @Override
